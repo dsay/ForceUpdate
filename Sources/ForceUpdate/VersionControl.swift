b75c668ca69d263de4e762d7bbc776a0e907df3b
Valentin Kovalski <valentin.kovalskyi@valtech.com>
2021-04-08 14:05:05 +0200

    any length version support

--- Sources/ForceUpdate/VersionControl.swift
+++ Sources/ForceUpdate/VersionControl.swift
@@ -1,55 +1,40 @@
 import Foundation
 
+extension Array {
+    func safeGet(at index: Int) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
+
 public struct Version: Codable {
     
-    public static var numbers: Int = 3
+    private(set) var components: [Int] = []
     
-    public var string: String
-    
-    public var major: Int
-    public var minor: Int
-    public var patch: Int
-    
-    public init(_ version: String) {
-        let components = Version.normalize(version, toNumber: Version.numbers)
-        
-        self.major = components[0]
-        self.minor = components[1]
-        self.patch = components[2]
-        self.string =  "\(major).\(minor).\(patch)"
+    public init(_ value: String) {
+        components = value
+            .split(separator: ".", omittingEmptySubsequences: false)
+            .compactMap({ Int($0) })
     }
     
     public init(_ version: Version) {
-        string = version.string
-        major = version.major
-        minor = version.minor
-        patch = version.patch
+        components = version.components
     }
     
-    public init(_ major: Int, _ minor: Int, _ patch: Int) {
-        self.string = "\(major).\(minor).\(patch)"
-        self.major = major
-        self.minor = minor
-        self.patch = patch
-    }
-    
-    private static func normalize(_ version: String, toNumber characters: Int) -> [Int] {
-        var components = version
-            .split(separator: ".", omittingEmptySubsequences: false)
-            .map(String.init)
-            .compactMap({ Int($0) })
-            .filter({ $0 >= 0 })
+    func compare(_ another: Version) -> ComparisonResult {
+        let maxCount = max(components.count, another.components.count)
         
-        let diff = characters - components.count
-        
-        if diff < 0 {
-            components = components.dropLast(abs(diff))
-        } else if diff < characters {
-            let zeros = Array(repeating: 0, count: abs(diff))
-            components.append(contentsOf: zeros)
+        var result = ComparisonResult.orderedSame
+        for index in 0..<maxCount {
+            let left = NSNumber(value: components.safeGet(at: index) ?? 0)
+            let right = NSNumber(value: another.components.safeGet(at: index) ?? 0)
+            
+            result = left.compare(right)
+            if result != .orderedSame {
+                return result
+            }
         }
         
-        return components
+       return result
     }
 }
 
@@ -63,14 +48,14 @@ extension Version: ExpressibleByStringLiteral {
 extension Version {
     
     public static func == (lhs: Version, rhs: Version) -> Bool {
-        lhs.string.compare(rhs.string) == .orderedSame
+        lhs.compare(rhs) == .orderedSame
     }
 
     public static func > (lhs: Version, rhs: Version) -> Bool {
-        lhs.string.compare(rhs.string) == .orderedDescending
+        lhs.compare(rhs) == .orderedDescending
     }
     
     public static func < (lhs: Version, rhs: Version) -> Bool {
-        lhs.string.compare(rhs.string) == .orderedAscending
+        lhs.compare(rhs) == .orderedAscending
     }
 }
